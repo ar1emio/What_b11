07b8340d63bd0a75383f8618ea39d34611d94e25
ar1emio <144154459+ar1emio@users.noreply.github.com>
2023-09-22 11:39:58 +0500

    Create index.php
    Практика №5

---
+++ pr5/index.php
@@ -0,0 +1,17 @@
+<?php
+    $host       = "db4.myarena.ru";       // Адрес сервера базы данных
+    $dbname     = "u19978_b11";           // Имя базы данных
+    $user       = "u19978_b11";           // Имя пользователя
+    $password   = "lL9bE1jS5v";               // Пароль
+    
+    try {
+        $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+        // Устанавливаем режим обработки ошибок PDO
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Подключение успешно";
+    } 
+    catch(PDOException $e) {
+        echo "Ошибка подключения: " . $e->getMessage();
+    }
+    ?>
+    
