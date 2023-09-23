082a2dc7a4bf6c7fcdbd1430ffc7161058d6151f
ar1emio <144154459+ar1emio@users.noreply.github.com>
2023-09-23 09:57:48 +0500

    Update index.php

--- pr5/index.php
+++ pr5/index.php
@@ -3,15 +3,19 @@
     $dbname     = "u19978_b11";           // Имя базы данных
     $user       = "u19978_b11";           // Имя пользователя
     $password   = "lL9bE1jS5v";               // Пароль
+    $connection = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $passsword);
     
-    try {
-        $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
-        // Устанавливаем режим обработки ошибок PDO
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Подключение успешно";
-    } 
-    catch(PDOException $e) {
-        echo "Ошибка подключения: " . $e->getMessage();
+    echo '<table tellspacing="1" bgcolor="#000000">';
+
+    $result = $connection->query('SELECT * FROM `product`');
+    while($row = $result->fetch( PDO::FETCH_ASSOC )){
+        echo '<tr bgcolor="#ffffff">';
+        foreach ($row as $key => $value) {
+        echo '<td>'.$value.'</td>';
     }
+    echo '</tr>';
+}
+echo '<table>';
+
     ?>
     
