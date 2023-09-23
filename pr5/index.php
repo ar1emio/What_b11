6232aa98d1401bf42d576e71c13453b81d75b5e3
ar1emio <144154459+ar1emio@users.noreply.github.com>
2023-09-23 10:47:33 +0500

    Update index.php

--- pr5/index.php
+++ pr5/index.php
@@ -3,19 +3,35 @@
     $dbname     = "u19978_b11";           // Имя базы данных
     $user       = "u19978_b11";           // Имя пользователя
     $password   = "lL9bE1jS5v";               // Пароль
-    $connection = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $passsword);
+    $connection = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
     
-    echo '<table tellspacing="1" bgcolor="#000000">';
-
     $result = $connection->query('SELECT * FROM `product`');
+
+    if ($result){
+        if ($result->rowCount() ==0){
+            echo "Таблица пустая";
+        } else {
+    
+    echo '<table border="1">';
+
+    echo "<tr>";
+    for($i - 0; $i < $result->columnCount();$i++){
+        $column = $result->getColumnMeta($i);
+        echo "<th>{$column['name']}</th>";
+    }
+    echo"</tr>";
+
     while($row = $result->fetch( PDO::FETCH_ASSOC )){
-        echo '<tr bgcolor="#ffffff">';
+        echo '<tr>';
         foreach ($row as $key => $value) {
         echo '<td>'.$value.'</td>';
     }
     echo '</tr>';
 }
 echo '<table>';
+        }}
+        else {
+            echo 'Запрос выполнен с ошибкой';
+        }
 
     ?>
-    
