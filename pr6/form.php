70cf058e3dac9a3eb4aef2a7098f7dd938404cf2
ar1emio <144154459+ar1emio@users.noreply.github.com>
2023-09-30 10:48:07 +0500

    Create form.php

---
+++ pr6/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+    <HTML lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>pr6</title>
+        </head>
+    <body>
+        <form action="action.php" method="post">
+            <h3>Форма ввода данных</h3>
+            <p>Ваше имя: <input type="text" name="name"></p>
+            <p>Ваш возраст: <input type="text" name="age"></p>
+            <p><input type="submit"></p> 
+        </form>
+    </body>
+</html>
