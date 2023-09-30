2649c845b663a9be4ca5642125cefef1785a8106
ar1emio <144154459+ar1emio@users.noreply.github.com>
2023-09-30 11:29:44 +0500

    Update action.php

--- pr7/action.php
+++ pr7/action.php
@@ -1,5 +1,4 @@
 <?php 
-    if( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
         if( $_SERVER['REQUEST_METHOD'] !== 'POST' ) exit;
         /*
          Запрос имеет тип POST, обрабатываем данные
@@ -8,11 +7,12 @@
         if(empty($_POST['login'])) exit('Поле "Логин" не заполнено');
         if(empty($_POST['password'])) exit('Поле "Пароль" не заполнено');
         if(empty($_POST['password1'])) exit('Поле "Подтвердите пароль" не заполнено');
+            if($_POST['password'] !== $_POST['password1']) exit('Пароли не совпадают');
         if(empty($_POST['email'])) exit('Поле "Почта" не заполнено');
 
 
         echo 'Здравствуйте! '.htmlspecialchars($_POST['name']).'. <br>';
         echo 'Ваш аккаунт создан!'.'<br>';
-    } 
+     
 ?>
 
