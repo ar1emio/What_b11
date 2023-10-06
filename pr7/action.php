655266fc7d97da90e1fa02ce283f536e428f1951
ar1emio <144154459+ar1emio@users.noreply.github.com>
2023-10-06 10:48:17 +0500

    Update action.php

--- pr7/action.php
+++ pr7/action.php
@@ -6,8 +6,10 @@
 
         if(empty($_POST['login'])) exit('Поле "Логин" не заполнено');
         if(empty($_POST['password'])) exit('Поле "Пароль" не заполнено');
+            $len = strlen($_POST['password']);
+            if($len<8) exit('В поле "пароль" введено мало символов'); 
         if(empty($_POST['password1'])) exit('Поле "Подтвердите пароль" не заполнено');
-            if($_POST['password'] !== $_POST['password1']) exit('Пароли не совпадают');
+        if($_POST['password'] !== $_POST['password1']) exit('Пароли не совпадают');
         if(empty($_POST['email'])) exit('Поле "Почта" не заполнено');
 
 
