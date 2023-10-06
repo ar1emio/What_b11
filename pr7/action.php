f1c579bbfba9655eb7462b286a2714c12480b002
ar1emio <144154459+ar1emio@users.noreply.github.com>
2023-10-06 11:39:40 +0500

    Update action.php

--- pr7/action.php
+++ pr7/action.php
@@ -1,20 +1,33 @@
 <?php 
+    $host       = "db4.myarena.ru";       // Адрес сервера базы данных
+    $dbname     = "u19978_b11";           // Имя базы данных
+    $user       = "u19978_b11";           // Имя пользователя
+    $password   = "lL9bE1jS5v";               // Пароль
+    $connection = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password); 
+        
         if( $_SERVER['REQUEST_METHOD'] !== 'POST' ) exit;
-        /*
-         Запрос имеет тип POST, обрабатываем данные
-        */ 
 
-        if(empty($_POST['login'])) exit('Поле "Логин" не заполнено');
-        if(empty($_POST['password'])) exit('Поле "Пароль" не заполнено');
-            $len = strlen($_POST['password']);
-            if($len<8) exit('В поле "пароль" введено мало символов'); 
-        if(empty($_POST['password1'])) exit('Поле "Подтвердите пароль" не заполнено');
-        if($_POST['password'] !== $_POST['password1']) exit('Пароли не совпадают');
-        if(empty($_POST['email'])) exit('Поле "Почта" не заполнено');
+            if(empty($_POST['login'])) exit('Поле "Логин" не заполнено');
+            if(empty($_POST['password'])) exit('Поле "Пароль" не заполнено');
 
+                $len = strlen($_POST['password']);
+                if($len<8) exit('В поле "пароль" введено мало символов'); 
+                if($len>20) exit('В поле "пароль" введено мало символов'); 
+                
+            if(empty($_POST['password1'])) exit('Поле "Подтвердите пароль" не заполнено');
+            if($_POST['password'] !== $_POST['password1']) exit('Пароли не совпадают');
+            if(empty($_POST['email'])) exit('Поле "Почта" не заполнено');
 
-        echo 'Здравствуйте! '.htmlspecialchars($_POST['name']).'. <br>';
-        echo 'Ваш аккаунт создан!'.'<br>';
+        $date = [$_POST['login'], $_POST['password'], $_POST['password1'], $_POST['email']];
+        $res = $con -> prepare ("INSERT INTO `users` (`login`, `password`, `password1`, `email`) VALUES (?,?,?,?);");
+        $res = $res -> execute ($date);
+
+        if( $res ){
+            exit('Регистрация прошла успешно');
+        }
+
+        exit('Ошибка регистрация');
+    
      
 ?>
 
