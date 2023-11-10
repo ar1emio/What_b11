1f54e187240e329c9a34c7ccaa4361477b62ee79
ar1emio <144154459+ar1emio@users.noreply.github.com>
2023-11-10 11:40:17 +0500

    Update script.js

--- JS/Практика №8/Задание №1-10/script.js
+++ JS/Практика №8/Задание №1-10/script.js
@@ -1,11 +1,17 @@
-let arr  = [
-    'Шайбель Артём Алексеевич',
-    'Студент',
-    'Среднее',
-    'Первая',
-    'ОНИКС',
-    'Обработка информации',
-    'Примечание отсутствует',
-];
+let arr  = {
+    name: 'Шайбель Артём Алексеевич',
+    ocupation: 'Студент',
+    degree: 'Среднее',
+    category: 'Первая',
+    education: 'ОНИКС',
+    discipline: 'Обработка информации',
+    note: 'Примечания отсутствуют',
+};
 
-
+document.write("<H2>" + arr.name + "</H2>");
+document.write("<b> Должность: </b>" + arr.ocupation + "<br>" + "<br>");
+document.write("<b> Уровень образования: </b>" + arr.degree + "<br>" + "<br>");
+document.write("<b> Категория: </b>" + arr.category + "<br>" + "<br>");
+document.write("<b> Образование: </b>" + arr.education + "<br>" + "<br>");
+document.write("<b> Преподаваемая дисциплина: </b>" + arr.discipline + "<br>" + "<br>");
+document.write("<b> Примечание: </b>" + arr.note + "<br>" + "<br>");
