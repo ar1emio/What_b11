e590d56b4a86eaa35c8901f4aaf47a54e0b0af42
ar1emio <144154459+ar1emio@users.noreply.github.com>
2023-11-10 09:54:02 +0500

    Create script.js

---
+++ JS/Практика №8/Задание №1-8/script.js
@@ -0,0 +1,11 @@
+let surname = 'Шайбель';
+let name = 'Артём';
+let patronymic = 'Алексеевич';
+let occupation = 'Студент';
+let degree = 'Хороший)';
+let category = 'Первая';
+let education = 'Среднее';
+let discipline = 'Обработка информации';
+let note = 'Примечание отсутствует';
+
+
