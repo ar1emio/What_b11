3fc5ae1b4857dc1cda5a0e0fe4ec501552648deb
ar1emio <artemsjbl@gmail.com>
2023-11-25 10:50:50 +0500

    Create 9.1.js

---
+++ JS/Практика №10/2/js/9.1.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  var input = document.getElementById("input");
+  var output = document.getElementById("output");
+  output.innerHTML = input.value;
+  output.style.display = "block";
+}
