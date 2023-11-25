8c90a2d1f144a2c122f09e9e954cea87bb155b25
ar1emio <artemsjbl@gmail.com>
2023-11-25 09:28:23 +0500

    Create 9.1.js

---
+++ JS/Практика №9/Задание 9.1/js/9.1.js
@@ -0,0 +1,74 @@
+let $header = document.createElement('h1');
+let $pr = document.createTextNode('Практика 9');        
+$header.append($pr);
+$header.className = 'header';
+document.body.append($header);
+
+let $h2 = document.createElement('h2');
+let $text = document.createTextNode('Lorem ipsum');  
+$h2.append($text);
+$h2.className = 'h2';
+document.body.append($h2);
+
+let $h3 = document.createElement('h3');
+let $text1 = document.createTextNode('Dolor sit amet');  
+$h3.append($text1);
+$h3.className = 'h3';
+document.body.append($h3);
+
+let $p = document.createElement('p');
+let $text2 = document.createTextNode('Aliquam eleifend consequat purus, a tincidunt odio vehicula eget. Sed sollicitudin, urna vitae elementum iaculis, leo tellus tristique tortor, quis condimentum justo sem sit amet sem. Fusce mattis metus metus, sed egestas tellus faucibus quis.');  
+$p.append($text2);
+$p.className = 'p';
+document.body.append($p);
+
+let $h = document.createElement('h3');
+let $text3 = document.createTextNode('Nunc a ante feugiat');  
+$h.append($text3);
+$h.className = 'h3';
+document.body.append($h);
+
+let $p1 = document.createElement('p');
+let $text4 = document.createTextNode('Nunc a ante feugiat, hendrerit nunc sed, tristique ipsum. In hac habitasse platea dictumst. Etiam dapibus mattis nulla a consequat. Phasellus nec nunc et dui condimentum tristique.');  
+$p1.append($text4);
+$p1.className = 'p1';
+document.body.append($p1);
+
+let $h1 = document.createElement('h3');
+let $text5 = document.createTextNode('Ut consequat auctor enim et dictum');  
+$h1.append($text5);
+$h1.className = 'h3';
+document.body.append($h1);
+
+let $p2 = document.createElement('p');
+let $text6 = document.createTextNode('Ut consequat auctor enim et dictum. Maecenas venenatis et diam porta egestas. Suspendisse id erat lorem. Aenean consequat magna vitae eleifend accumsan. Suspendisse rhoncus pulvinar interdum. Aliquam vitae diam sed massa fermentum elementum.');  
+$p2.append($text6);
+$p2.className = 'p2';
+document.body.append($p2);
+
+let $h4 = document.createElement('h3');
+let $text7 = document.createTextNode('Sed ultricies mauris quam');  
+$h4.append($text7);
+$h4.className = 'h3';
+document.body.append($h4);
+
+let $p3 = document.createElement('p');
+let $text8 = document.createTextNode('Sed ultricies mauris quam, vel feugiat leo iaculis vitae. Donec nulla ex, maximus ac dapibus posuere, sagittis at nisi.');  
+$p3.append($text8);
+$p3.className = 'p3';
+document.body.append($p3);
+
+let $footer = document.createElement('footer');
+let $text9 = document.createTextNode('Практику выполнил: Шайбель Артём');  
+$footer.append($text9);
+$footer.className = 'footer';
+document.body.append($footer);
+
+let $br = document.createElement('br');
+document.body.append($br);
+
+let $foooter1 = document.createElement('footer');
+let $text10 = document.createTextNode('© ГБПОУ "Пермский краевой колледж "ОНИКС"');  
+$foooter1.append($text10);
+$foooter1.className = 'footer';
+document.body.append($foooter1);
