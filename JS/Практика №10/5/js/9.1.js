0cd88c897dc169f421e5222bcdc8c526a12b3c22
ar1emio <artemsjbl@gmail.com>
2023-11-25 10:51:25 +0500

    Create 9.1.js

---
+++ JS/Практика №10/5/js/9.1.js
@@ -0,0 +1,38 @@
+function myFunction() {
+  var input = document.getElementById("input");
+  var output = document.getElementById("output");
+  output.innerHTML = input.value;
+  output.style.display = "block";
+}
+function myFunction1() {
+  var output1 = document.getElementById("output1");
+  output1.innerHTML = "Нажата первая кнопка";
+  output1.style.display = "block";
+}
+
+function myFunction2() {
+  var output2 = document.getElementById("output2");
+  output2.innerHTML = "Нажата вторая кнопка";
+  output2.style.display = "block";
+}
+var block = document.querySelector(".myBlock");
+
+block.addEventListener("mouseover", function() {
+  this.style.transition = "all 0.5s ease";
+  this.style.backgroundColor = "#f44361";
+});
+
+block.addEventListener("mouseout", function() {
+  this.style.transition = "all 0.5s ease";
+  this.style.backgroundColor = "#fff";
+});
+
+function showDetails() {
+  var details = document.getElementById("details");
+  details.style.display = "block";
+}
+
+function hideDetails() {
+  var details = document.getElementById("details");
+  details.style.display = "none";
+}
